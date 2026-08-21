600d08a050be0114c9ba9ad19a189e3f31ed7b00
PenguineWalkOS <slavazeph@gmail.com>
2026-08-21 19:41:43 -0400

    Load Arcade homepage discovery styles

--- brainsnn-r3f-app/src/main.tsx
+++ brainsnn-r3f-app/src/main.tsx
@@ -4,6 +4,7 @@ import App from './App.tsx';
 import './index.css';
 import './styles/tokens.css';
 import './styles/utilities.css';
+import './styles/arcade-home.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
